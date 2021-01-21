23a9f45faebc55d932844b77b79c1c44e6a9ce01
Khalid Ahmed <csewebmail@gmail.com>
2021-01-22 01:20:31 +0600

    create token class

--- public/js/app.js
+++ public/js/app.js
@@ -54954,8 +54954,8 @@ var routes = [{
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\shop70\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\shop70\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\shop7.0\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\shop7.0\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
